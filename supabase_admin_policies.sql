b9db98a6507c8c142a408f9fb4f84a9f90d9832f
Laurence Peter <peterlaw.dev@gmail.com>
2026-01-30 23:15:10 -0400

    Fix admin function search_path

--- supabase_admin_policies.sql
+++ supabase_admin_policies.sql
@@ -4,7 +4,7 @@ create or replace function public.is_admin()
 returns boolean
 language sql
 security definer
-set search_path = public
+set search_path = ''
 as $$
   select (auth.jwt() ->> 'app_role') = 'admin'
     or exists (
